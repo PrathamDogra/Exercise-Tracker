5eed29609f6ac298630cc00331a0a95fccc76722
Pratham Dogra <dogra.pratham@gmail.com>
2019-10-07 21:13:24 +0530

    Building for production

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors=require('cors')
 const app = express();
+const path =require('path');
 require('dotenv').config();
 //As body-parser is inside the express
 app.use(express.json());
@@ -9,7 +10,7 @@ app.use(cors());
 
 const uri = process.env.ATLAS_URI;
 mongoose
-  .connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+  .connect(process.env.MONGODB_URI || uri, { useNewUrlParser: true, useCreateIndex: true })
   .then(() =>
     console.log("The connection to MongoDB is established successfully")
   )
@@ -22,5 +23,14 @@ app.use('/exercises', exerciseRoute);
 app.get("/app", (req, res) => {
   res.status(200).json("Hello World");
 });
-port = 5000;
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('build'));
+  app.get('*',(req,res) =>{
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  })
+}
+
+
+port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running at ${port}`));
