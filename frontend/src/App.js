4602bd08619617d2c349438c969eb238bec25fea
Pratham Dogra <dogra.pratham@gmail.com>
2019-10-08 20:44:49 +0530

    Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <footer className="footer mt-auto">
+        <div className="container">
+          <p align="center">
+            Made with ❤️ and ☕️ by{" "}
+            <a href="https://www.linkedin.com/in/prathamdogra/" target="_blank">
+              Pratham Dogra
+            </a>{" "}
+            |{" "}
+            <a
+              href="https://github.com/PrathamDogra/Exercise-Tracker"
+              target="_blank"
+            >
+              GitHub Repo
+            </a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
